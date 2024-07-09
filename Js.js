9c8f070ca127547a6210ec42dc2a93b91e4ec716
otmane ibbour <otmane.ibbour@usmba.ac.ma>
2024-07-09 21:27:51 +0200

    my web site

---
+++ Js.js
@@ -0,0 +1,6 @@
+let array = [{name:'otman',surname:'ibbour'},{name:'kawtar',surname:'ibbour'},{name:'malak',surname:'ibbour'}]
+
+array.find((item)=>{
+    return item.name === 'kawtar';
+})
+// console.log(arr)
